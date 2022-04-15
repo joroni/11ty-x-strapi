e49eb1b7d3a24bdf5f71d58bdf6a2296a9e84dff
joroni <jraymund.niconi@gmail.com>
2022-04-15 09:15:46 +0800

    Update

--- frontend/_data/blogposts.js
+++ frontend/_data/blogposts.js
@@ -2,30 +2,30 @@ const fetch = require("node-fetch");
 
 // function to get blogposts
 async function getAllBlogposts() {
-  // max number of records to fetch per query
-  const recordsPerQuery = 100;
+    // max number of records to fetch per query
+    const recordsPerQuery = 100;
 
-  // number of records to skip (start at 0)
-  let recordsToSkip = 0;
+    // number of records to skip (start at 0)
+    let recordsToSkip = 0;
 
-  // do we make a query ?
-  let makeNewQuery = true;
+    // do we make a query ?
+    let makeNewQuery = true;
 
-  // Blogposts array
-  let blogposts = [];
+    // Blogposts array
+    let blogposts = [];
 
-  // make queries until makeNewQuery is set to false
-  while (makeNewQuery) {
-    try {
-      // initiate fetch
-      const data = await fetch("https://okaaaaaaayyy.herokuapp.com/graphql", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          query: `{
+    // make queries until makeNewQuery is set to false
+    while (makeNewQuery) {
+        try {
+            // initiate fetch
+            const data = await fetch("http://localhost:1337/graphql", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify({
+                    query: `{
               articles {
               id
               title
@@ -35,50 +35,50 @@ async function getAllBlogposts() {
               slug
             }
           }`,
-        }),
-      });
+                }),
+            });
 
-      // store the JSON response when promise resolves
-      const response = await data.json();
+            // store the JSON response when promise resolves
+            const response = await data.json();
 
-      // handle CMS errors
-      if (response.errors) {
-        let errors = response.errors;
-        errors.map((error) => {
-          console.log(error.message);
-        });
-        throw new Error("Houston... We have a CMS problem");
-      }
+            // handle CMS errors
+            if (response.errors) {
+                let errors = response.errors;
+                errors.map((error) => {
+                    console.log(error.message);
+                });
+                throw new Error("Houston... We have a CMS problem");
+            }
 
-      // update blogpost array with the data from the JSON response
-      blogposts = blogposts.concat(response.data.articles);
+            // update blogpost array with the data from the JSON response
+            blogposts = blogposts.concat(response.data.articles);
 
-      // prepare for next query
-      recordsToSkip += recordsPerQuery;
+            // prepare for next query
+            recordsToSkip += recordsPerQuery;
 
-      // stop querying if we are getting back less than the records we fetch per query
-      if (response.data.articles.length < recordsPerQuery) {
-        makeNewQuery = false;
-      }
-    } catch (error) {
-      throw new Error(error);
+            // stop querying if we are getting back less than the records we fetch per query
+            if (response.data.articles.length < recordsPerQuery) {
+                makeNewQuery = false;
+            }
+        } catch (error) {
+            throw new Error(error);
+        }
     }
-  }
 
-  // format blogposts objects
-  const blogpostsFormatted = blogposts.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      slug: item.slug,
-      body: item.content,
-      author: item.author,
-      date: item.published_at       
-    };
-  });
+    // format blogposts objects
+    const blogpostsFormatted = blogposts.map((item) => {
+        return {
+            id: item.id,
+            title: item.title,
+            slug: item.slug,
+            body: item.content,
+            author: item.author,
+            date: item.published_at
+        };
+    });
 
-  // return formatted blogposts
-  return blogpostsFormatted;
+    // return formatted blogposts
+    return blogpostsFormatted;
 }
 
 // export for 11ty
